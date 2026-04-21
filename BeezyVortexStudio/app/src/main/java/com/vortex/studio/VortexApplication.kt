0c3d2533aca0940cbc26061996b509a9528cf031
alshihaf2 <alshihaf2@gmail.com>
2026-04-21 08:45:07 +0000

    Changes before Firebase Studio auto-run

---
+++ BeezyVortexStudio/app/src/main/java/com/vortex/studio/VortexApplication.kt
@@ -0,0 +1,11 @@
+
+package com.vortex.studio
+
+import android.app.Application
+
+class VortexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Application-level initializations go here.
+    }
+}
